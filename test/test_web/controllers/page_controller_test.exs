48a4b6724adb5488687febba1ce11a347a7fce95
Jonathan Arnett <jonarnett90@gmail.com>
2019-02-10 16:53:25 -0500

    Cleanup homepage

--- test/test_web/controllers/page_controller_test.exs
+++ test/test_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TestWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to J3RN's Test Phoenix Application!"
   end
 end
