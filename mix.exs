0cedef299f3c51cb798f3833fa205e4765fee420
Jonathan Arnett <jonarnett90@gmail.com>
2019-02-10 23:56:06 -0500

    Add authentication dependencies

--- mix.exs
+++ mix.exs
@@ -43,6 +43,8 @@ defmodule Test.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.1"},
+      {:pbkdf2_elixir, "~> 0.12.3"},
 
       # Test deps
       {:ex_machina, "~> 2.2", only: :test}
