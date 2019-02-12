ee0d74f1f4a2fa5be45e513e28ee7f05884cea48
Jonathan Arnett <jonarnett90@gmail.com>
2019-02-11 23:02:13 -0500

    Implement SessionContoller.create and delete

--- lib/test_web/controllers/session_controller.ex
+++ lib/test_web/controllers/session_controller.ex
@@ -1,13 +1,28 @@
 defmodule TestWeb.SessionController do
   use TestWeb, :controller
+  alias Test.Pharmacies
 
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
-  def create(conn, %{"session" => %{name: name, password: password}}) do
-    conn
+  def create(conn, %{"session" => %{"name" => name, "password" => password}}) do
+    case Pharmacies.authenticate_pharmacy(name, password) do
+      {:ok, pharmacy} ->
+        conn
+        |> TestWeb.Auth.login(pharmacy)
+        |> redirect(to: Routes.order_path(conn, :index))
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid credentials")
+        |> render("new.html")
+    end
   end
 
-  def delete(conn, _params), do: conn
+  def delete(conn, _params) do
+    conn
+    |> TestWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
 end
